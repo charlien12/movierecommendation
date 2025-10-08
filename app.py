711e18e5447fd4ee23b4fbfa1ef35c7c45326819
8204 <cnegi@raa.com.au>
2025-10-08 12:17:23 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,24 +1,114 @@
 import streamlit as st
+
 import pickle
+
 import pandas as pd
-st.title("Movie Recommendation System")
-st.write("This is a simple movie recommendation system.")
-movies=pickle.load(open('movies_dict.pkl','rb'))
-similarity=pickle.load(open('similarity.pkl','rb'))
-movies_df=pd.DataFrame(movies)
-movie_list=movies_df['title'].values
-selected_movie=st.selectbox("Type or select a movie from the dropdown",movie_list)
+
+import requests
+
+# -----------------------------
+
+# Function to fetch movie poster
+
+# -----------------------------
+
+def fetch_poster(movie_id):
+
+    try:
+
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+
+        data = requests.get(url)
+
+        data.raise_for_status()
+
+        data = data.json()
+
+        poster_path = data.get('poster_path')
+
+        if poster_path:
+
+            full_path = f"https://image.tmdb.org/t/p/w500/{poster_path}"
+
+            return full_path
+
+        else:
+
+            return "https://via.placeholder.com/500x750?text=No+Image"
+
+    except Exception as e:
+
+        return "https://via.placeholder.com/500x750?text=No+Image"
+
+# -----------------------------
+
+# Streamlit UI
+
+# -----------------------------
+
+st.title("🎬 Movie Recommendation System")
+
+st.write("Select a movie and get similar recommendations!")
+
+# Load data
+
+movies = pickle.load(open('movies_dict.pkl', 'rb'))
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+movies_df = pd.DataFrame(movies)
+
+# Dropdown list
+
+movie_list = movies_df['title'].values
+
+selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
+
+# -----------------------------
+
+# Recommendation function
+
+# -----------------------------
+
 def recommend(movie):
-    movie_index=movies_df[movies_df['title']==movie].index[0]
-    distances=similarity[movie_index]
-    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
-    recommended_movies=[]
+
+    movie_index = movies_df[movies_df['title'] == movie].index[0]
+
+    distances = similarity[movie_index]
+
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+
+    recommended_movie_posters = []
+
     for i in movies_list:
+
+        movie_id = movies_df.iloc[i[0]].movie_id
+
         recommended_movies.append(movies_df.iloc[i[0]].title)
-    return recommended_movies
+
+        recommended_movie_posters.append(fetch_poster(movie_id))
+
+    return recommended_movies, recommended_movie_posters
+
+# -----------------------------
+
+# Display recommendations
+
+# -----------------------------
 
 if st.button('Show Recommendation'):
-    recommendations=recommend(selected_movie)
-    for i in recommendations:
-        st.write(i)
-# You can replace 'Avatar' with any movie title present in your dataset to get recommendations.
+
+    recommended_movies, recommended_movie_posters = recommend(selected_movie)
+
+    cols = st.columns(5)
+
+    for idx, col in enumerate(cols):
+
+        with col:
+
+            st.text(recommended_movies[idx])
+
+            st.image(recommended_movie_posters[idx])
+ 
